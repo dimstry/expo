d4dec9506dd8b29fd930871bd8a38fc0eb4c602b
dimstry <dimastriana03@gmail.com>
2022-09-30 16:20:22 +0700

    tst

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World!!</Text>
+      {/* membuat button  */}
       <StatusBar style="auto" />
+      <Button title="Click Me" onPress={() => alert("Hello World!!")} />
+      {/* Membuat footer */}
+      <View style={styles.footer}>
+        <Text style={styles.textFooter}>My Footer</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +20,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  footer: {
+    backgroundColor: "red",
+    width: "100%",
+    height: 50,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    bottom: 0,
+  },
+  textFooter: {
+    color: "white",
   },
 });
